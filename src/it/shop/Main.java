99b93fece186587c51dc9e3e9ca58b2674624e8a
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-25 16:41:05 +0100

    secondo commit

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -3,8 +3,15 @@ package it.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		
 
+		String nome = "Maglietta";
+		String descrizione = "Bella e Calda";
+		int prezzo = 30;
+		int iva = 22;
+
+		Prodotto prodotto = new Prodotto(prezzo, nome, descrizione, iva);
+
+		System.out.println("Il prodotto è: " + prodotto.nomeCodice() + "\nLa sua descrizione è: " + prodotto.getDescrizione() + "\nIl suo prezzo è: "
+				+ prodotto.getPrezzo() + "\nIl prezzo con l'IVA è: " + prodotto.prezzoIva());
 	}
-
 }
--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -1,5 +1,101 @@
 package it.shop;
 
+import java.util.Random;
+
 public class Prodotto {
+
+	/*
+	 * alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+Il codice sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura 
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+	 * 
+	 */
+
+
+	////////////  PROPRIETA ///////////////////////////////////
+
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private int prezzo;
+	private int iva;
+
+	/////////////// COSTRUTTORE ///////////////////////////////
+
+	public Prodotto (int prezzo, String nome, String descrizione, int iva) {
+
+		this.codice = generaCodice();	
+		setIva(22);		
+		setPrezzo(prezzo);
+		setNome(nome);
+		setDescrizione(descrizione);
+
+	}
+
+
+
+
+
+
+	///////////// SGETTER /////////////////////////////////	
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {		
+		this.nome = nome;
+	}
+
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public int getPrezzo() {		
+		return prezzo;
+	}
+
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public int getIva() {
+		return iva;
+	}
+
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+
+
+	////////////////// METODI //////////////////////////////
+
+	//////CREAZIONE CODICE 
+	private int generaCodice() {
+		Random c = new Random ();
+		return c.nextInt(501);
+	}
+
+	////////////////// PREZZO BASE + PREZZO IVA
+	public int prezzoIva() {
+		return (prezzo * iva)/100 + prezzo;
+	}
+	/////////////////NOME+CODICE
+	public String nomeCodice() {
+		return (nome + codice);
+	}
 
 }
